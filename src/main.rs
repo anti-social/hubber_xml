ce7fb0f0b1d8758d23bbde73e98ad44a88a8654d
Alexander Koval <kovalidis@gmail.com>
2019-12-21 01:21:45 +0200

    Update price and available fields

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ extern crate diesel;
 use diesel::prelude::*;
 use diesel::mysql::MysqlConnection;
 
-use dotenv::dotenv;
+use dotenv;
 
-use log::{info, warn};
+use log::{error, info, warn, LevelFilter};
 
 use std::env;
 use std::path::Path;
@@ -20,20 +20,38 @@ mod models;
 mod schema;
 
 use schema::products;
-//use schema::products::dsl::*;
+use std::collections::HashMap;
+
+#[derive(Default, Debug)]
+struct ProcessedStat {
+    pub total_offers: u32,
+    pub ignored_offers: u32,
+    pub parsed_offers: u32,
+    pub updated_products: u32,
+    pub inserted_products: u32,
+}
 
 fn main() -> Result<(), Error> {
+    env_logger::builder()
+        .filter(None, LevelFilter::Info)
+        .init();
+
+    let args: Vec<String> = env::args().collect();
+    let file_path = match args.get(1) {
+        Some(p) => Path::new(p),
+        None => return Err(format_err!("Expected exactly 1 argument: FILE_PATH")),
+    };
+
     let conn = establish_connection();
 
-    let (total_offers, processed_offers) = process_offers(Path::new("hubber.xml"), &conn)?;
-    println!("Total offers: {}", total_offers);
-    println!("Processed offers: {}", processed_offers);
+    let stat = process_offers(file_path, &conn)?;
+    info!("{:?}", stat);
 
     Ok(())
 }
 
 pub fn establish_connection() -> MysqlConnection {
-    dotenv().ok();
+    dotenv::dotenv().ok();
 
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set");
@@ -90,12 +108,11 @@ enum OfferFields {
     VendorCode,
 }
 
-fn process_offers(path: &Path, conn: &MysqlConnection) -> Result<(u32, u32), Error> {
+fn process_offers(path: &Path, conn: &MysqlConnection) -> Result<ProcessedStat, Error> {
     let mut xml_reader = Reader::from_file(path)?;
     let mut buf = vec!();
     let mut offer_buf = vec!();
-    let mut total_offers = 0;
-    let mut processed_offers = 0;
+    let mut stat = ProcessedStat::default();
 
     let mut products_bucket = vec!();
 
@@ -174,7 +191,7 @@ fn process_offers(path: &Path, conn: &MysqlConnection) -> Result<(u32, u32), Err
                                             if let Ok(price) = value.parse() {
                                                 offer.price = Some(price);
                                             } else {
-                                                warn!("Cannot parse price: {}", value);
+                                                warn!("{}: Cannot parse price: {}", offer.offer_id, value);
                                             }
                                         }
                                         OfferFields::OldPrice => {
@@ -185,11 +202,18 @@ fn process_offers(path: &Path, conn: &MysqlConnection) -> Result<(u32, u32), Err
                                                 "UAH" | "USD" | "EUR" | "RUB" | "BYR" | "KZT" => {
                                                     offer.currency_id = Some(value.to_string());
                                                 }
-                                                _ => {}
+                                                "" => {}
+                                                _ => {
+                                                    warn!("{}: Unknown currencyId: {}", offer.offer_id, value);
+                                                }
                                             }
                                         }
                                         OfferFields::CategoryId => {
-                                            offer.category_id = value.parse().ok();
+                                            if let Ok(cat_id) = value.parse() {
+                                                offer.category_id = Some(cat_id);
+                                            } else {
+                                                warn!("{}: Cannot parse categoryId: {}", offer.offer_id, value);
+                                            }
                                         }
                                         OfferFields::Name => {
                                             offer.name = Some(value.to_string());
@@ -219,19 +243,27 @@ fn process_offers(path: &Path, conn: &MysqlConnection) -> Result<(u32, u32), Err
                                 Ok(Event::Eof) => {
                                     unreachable!();
                                 }
+                                Err(e) => {
+                                    error!("Error at position: {}", xml_reader.buffer_position());
+                                    Err(e)?;
+                                }
                                 _ => {}
                             }
                         }
 
-                        total_offers += 1;
+                        stat.total_offers += 1;
                         if let Some(product) = convert_offer_to_product(offer) {
                             products_bucket.push(product);
-                            processed_offers += 1;
+                            stat.parsed_offers += 1;
+                        } else {
+                            stat.ignored_offers += 1;
                         }
                         if products_bucket.len() == 1000 {
-                            process_products_chunk(conn, &products_bucket)?;
+                            let (updated_count, inserted_count) = process_products_chunk(conn, &products_bucket)?;
+                            stat.updated_products += updated_count;
+                            stat.inserted_products += inserted_count;
                             products_bucket.clear();
-                            info!("Processed {} offers", total_offers)
+
                         }
                     }
                     _ => {}
@@ -241,32 +273,30 @@ fn process_offers(path: &Path, conn: &MysqlConnection) -> Result<(u32, u32), Err
                 break;
             }
             Err(e) => {
+                error!("Error at position: {}", xml_reader.buffer_position());
                 Err(e)?;
             }
             _ => {}
         }
     }
 
-    Ok((total_offers, processed_offers))
+    Ok(stat)
 }
 
 fn convert_offer_to_product(offer: Offer) -> Option<models::NewProduct> {
     let name = if let Some(name) = offer.name {
         name
     } else {
-        println!("no name");
         return None;
     };
     let category_id = if let Some(cat_id) = offer.category_id {
         cat_id
     } else {
-        println!("no category");
         return None;
     };
     let price = if let Some(price) = offer.price {
         price
     } else {
-        println!("no price");
         return None;
     };
     Some(models::NewProduct {
@@ -281,13 +311,58 @@ fn convert_offer_to_product(offer: Offer) -> Option<models::NewProduct> {
     })
 }
 
-fn process_products_chunk(conn: &MysqlConnection, products: &Vec<models::NewProduct>) -> Result<(), Error> {
-//    diesel::insert_into(products::table)
-//        .values(products)
-//        .execute(conn)?;
-    Ok(())
-}
+fn process_products_chunk(
+    conn: &MysqlConnection, parsed_products: &Vec<models::NewProduct>
+) -> Result<(u32, u32), Error> {
+    use schema::products::dsl::{products as products_table};
 
-//fn parse_offer(xml_reader: &mut Reader<BufReader<File>>, buf: &mut Vec<u8>) {
-//
-//}
+    let mut updated_count = 0;
+    let mut inserted_count = 0;
+
+    let offer_ids = parsed_products.iter()
+        .map(|p| p.offer_id.as_str())
+        .collect::<Vec<_>>();
+    let found_products = products_table
+        .filter(schema::products::offer_id.eq_any(offer_ids))
+        .load::<models::Product>(conn)?;
+    let offer_id_to_found_product = found_products.iter()
+        .map(|p| (p.offer_id.as_str(), p))
+        .collect::<HashMap<_, _>>();
+
+    for p in parsed_products {
+        match offer_id_to_found_product.get(p.offer_id.as_str()) {
+            Some(found_product) => {
+                let mut should_update = false;
+                let mut update_product = models::ModProduct::default();
+                if p.available != found_product.available {
+                    update_product.available = Some(p.available.as_ref());
+                    should_update = true;
+                }
+                if p.price != found_product.price || p.currencyId != found_product.currencyId {
+                    update_product.price = Some(&p.price);
+                    update_product.currencyId = Some(p.currencyId.as_deref());
+                    should_update = true;
+                }
+                if should_update {
+                    // println!("Updating product with offer_id={}: {:?}", p.offer_id, update_product);
+                    diesel::update(schema::products::table.find(found_product.id))
+                        .set(&update_product)
+                        .execute(conn)?;
+                    updated_count += 1;
+                }
+            }
+            None => {}
+        }
+    }
+
+    let insert_products = parsed_products.iter()
+        .filter(|&p| !offer_id_to_found_product.contains_key(p.offer_id.as_str()))
+        .collect::<Vec<_>>();
+    inserted_count += insert_products.len();
+    if !insert_products.is_empty() {
+        diesel::insert_into(products::table)
+            .values(insert_products)
+            .execute(conn)?;
+    }
+    Ok((updated_count, inserted_count as u32))
+}
--- src/models.rs
+++ src/models.rs
@@ -12,3 +12,28 @@ pub struct NewProduct {
     pub available: Option<i8>,
     pub description: Option<String>,
 }
+
+#[derive(Queryable)]
+pub struct Product {
+    pub id: i32,
+    pub offer_id: String,
+    pub categoryId: i32,
+    pub name: String,
+    pub price: f32,
+    pub oldprice: Option<f32>,
+    pub currencyId: Option<String>,
+    pub available: Option<i8>,
+    pub description: Option<String>,
+}
+
+#[derive(AsChangeset, Default, Debug)]
+#[table_name="products"]
+pub struct ModProduct<'a> {
+    pub price: Option<&'a f32>,
+    pub available: Option<Option<&'a i8>>,
+    pub currencyId: Option<Option<&'a str>>,
+//    pub categoryId: Option<&'a i32>,
+//    pub name: Option<&'a str>,
+//    pub oldprice: Option<&'a Option<f32>>,
+//    pub description: Option<&'a Option<String>>,
+}
