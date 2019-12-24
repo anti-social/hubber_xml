8b17e87171a772e046425d6cf316d5f4d5b6484f
Alexander Koval <kovalidis@gmail.com>
2019-12-24 13:04:46 +0200

    Split into modules

---
+++ src/parser.rs
@@ -0,0 +1,298 @@
+use chrono::Utc;
+
+use diesel::mysql::MysqlConnection;
+
+use failure::Error;
+
+use indicatif::{ProgressBar, ProgressStyle};
+
+use log::{error, warn};
+
+use quick_xml::Reader;
+use quick_xml::events::Event;
+
+use std::collections::HashSet;
+use std::fs;
+use std::time::{Duration, Instant};
+
+use crate::{Opts, ProcessedStat};
+use crate::models::{AVAILABLE, NOT_AVAILABLE};
+use crate::process::{convert_offer_to_product, mark_missing_as_unavailable, sync_products_chunk};
+
+pub(crate) struct Offer {
+    pub offer_id: String,
+    pub available: i8,
+    pub price: Option<f32>,
+    pub old_price: Option<f32>,
+    pub currency_id: Option<String>,
+    pub category_id: Option<i32>,
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub vendor: Option<String>,
+    pub vendor_code: Option<String>,
+}
+
+impl Offer {
+    pub fn new(offer_id: String, available: i8) -> Offer {
+        Offer {
+            offer_id,
+            available,
+            price: None,
+            old_price: None,
+            currency_id: None,
+            category_id: None,
+            name: None,
+            description: None,
+            vendor: None,
+            vendor_code: None,
+        }
+    }
+}
+
+enum OfferFields {
+    None,
+    Price,
+    OldPrice,
+    CurrencyId,
+    CategoryId,
+    Name,
+    Description,
+    Vendor,
+    VendorCode,
+}
+
+pub(crate) fn parse_offers(
+    opts: &Opts, conn: &MysqlConnection,
+) -> Result<ProcessedStat, Error> {
+    let start_processing_at = Instant::now();
+    let mut total_sync_duration = Duration::default();
+    let file_path = opts.file_path.as_path();
+    let file_size = fs::metadata(file_path)?.len();
+    let update_progress_after_chunk = file_size / 100;
+    let progress_bar = if !opts.no_progress {
+        let pb = ProgressBar::new(file_size);
+        pb.set_style(
+            ProgressStyle::default_bar()
+                .template("[{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+                .progress_chars("#>-")
+        );
+        Some(pb)
+    } else {
+        None
+    };
+    let mut xml_reader = Reader::from_file(file_path)?;
+    let mut buf = vec!();
+    let mut offer_buf = vec!();
+    let mut stat = ProcessedStat::default();
+
+    let mut products_bucket = vec!();
+    let mut all_offer_ids = HashSet::new();
+
+    let date_modified = Utc::now().naive_utc();
+
+    loop {
+        match xml_reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) |
+            Ok(Event::Empty(ref e)) => {
+                match e.name() {
+                    b"offer" => {
+                        let mut offer_id = None;
+                        let mut available = NOT_AVAILABLE;
+                        for attr_res in e.attributes() {
+                            let attr = attr_res?;
+                            match attr.key {
+                                b"id" => {
+                                    offer_id = Some(String::from_utf8_lossy(&attr.value).to_string());
+                                }
+                                b"available" => {
+                                    available = match attr.value.as_ref() {
+                                        b"" => NOT_AVAILABLE,
+                                        b"true" | b"1" => AVAILABLE,
+                                        b"false" | b"0" => NOT_AVAILABLE,
+                                        v => {
+                                            return Err(format_err!(
+                                                "Unknown \"available\" attribute: {}", String::from_utf8_lossy(v)
+                                            ))
+                                        }
+                                    };
+                                }
+                                _ => {}
+                            }
+                        }
+                        let mut offer = if let Some(offer_id) = offer_id {
+                            Offer::new(offer_id, available)
+                        } else {
+                            warn!("An offer without id was found");
+                            continue;
+                        };
+                        let mut offer_field = OfferFields::None;
+
+                        loop {
+                            match xml_reader.read_event(&mut offer_buf) {
+                                Ok(Event::Start(ref offer_event)) => {
+                                    match offer_event.name() {
+                                        b"price" => {
+                                            offer_field = OfferFields::Price;
+                                        }
+                                        b"oldprice" => {
+                                            offer_field = OfferFields::OldPrice;
+                                        }
+                                        b"currencyId" => {
+                                            offer_field = OfferFields::CurrencyId;
+                                        }
+                                        b"categoryId" => {
+                                            offer_field = OfferFields::CategoryId;
+                                        }
+                                        b"name" => {
+                                            offer_field = OfferFields::Name;
+                                        }
+                                        b"description" => {
+                                            offer_field = OfferFields::Description;
+                                        }
+                                        b"vendor" => {
+                                            offer_field = OfferFields::Vendor;
+                                        }
+                                        b"vendorCode" => {
+                                            offer_field = OfferFields::VendorCode;
+                                        }
+                                        _ => {}
+                                    }
+                                }
+                                Ok(Event::Text(ref v)) => {
+                                    let value = String::from_utf8_lossy(v.escaped());
+                                    match offer_field {
+                                        OfferFields::Price => {
+                                            if let Ok(price) = value.parse() {
+                                                offer.price = Some(price);
+                                            } else {
+                                                warn!("{}: Cannot parse price: {}", offer.offer_id, value);
+                                            }
+                                        }
+                                        OfferFields::OldPrice => {
+                                            offer.old_price = value.parse().ok();
+                                        }
+                                        OfferFields::CurrencyId => {
+                                            match value.as_ref() {
+                                                "UAH" | "USD" | "EUR" | "RUB" | "BYR" | "KZT" => {
+                                                    offer.currency_id = Some(value.to_string());
+                                                }
+                                                "" => {
+                                                    offer.currency_id = Some("UAH".to_string());
+                                                }
+                                                _ => {
+                                                    warn!("{}: Unknown currencyId: {}", offer.offer_id, value);
+                                                }
+                                            }
+                                        }
+                                        OfferFields::CategoryId => {
+                                            if let Ok(cat_id) = value.parse() {
+                                                offer.category_id = Some(cat_id);
+                                            } else {
+                                                warn!("{}: Cannot parse categoryId: {}", offer.offer_id, value);
+                                            }
+                                        }
+                                        OfferFields::Name => {
+                                            offer.name = Some(value.to_string());
+                                        }
+                                        OfferFields::Description => {
+                                            offer.description = Some(value.to_string());
+                                        }
+                                        OfferFields::Vendor => {
+                                            offer.vendor = Some(value.to_string());
+                                        }
+                                        OfferFields::VendorCode => {
+                                            offer.vendor_code = Some(value.to_string());
+                                        }
+                                        _ => {}
+                                    }
+                                }
+                                Ok(Event::End(ref e)) => {
+                                    match e.name() {
+                                        b"offer" => {
+                                            break;
+                                        }
+                                        _ => {
+                                            offer_field = OfferFields::None;
+                                        }
+                                    }
+                                }
+                                Ok(Event::Eof) => {
+                                    unreachable!();
+                                }
+                                Err(e) => {
+                                    error!("Error at position: {}", xml_reader.buffer_position());
+                                    Err(e)?;
+                                }
+                                _ => {}
+                            }
+
+                            offer_buf.clear();
+                        }
+
+                        stat.total_offers += 1;
+                        if let Some(product) = convert_offer_to_product(offer) {
+                            if opts.mark_missing_unavailable {
+                                all_offer_ids.insert(product.offer_id.clone());
+                            }
+                            products_bucket.push(product);
+                            stat.parsed_offers += 1;
+                        } else {
+                            stat.ignored_offers += 1;
+                        }
+                        if products_bucket.len() == 1000 {
+                            let processed_products_stat = sync_products_chunk(
+                                conn, &products_bucket, opts, &date_modified
+                            )?;
+                            stat.updated_price += processed_products_stat.updated_price;
+                            stat.updated_available += processed_products_stat.updated_available;
+                            stat.inserted_products += processed_products_stat.inserted;
+                            total_sync_duration += processed_products_stat.duration;
+                            products_bucket.clear();
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            Ok(Event::Eof) => {
+                break;
+            }
+            Err(e) => {
+                error!("Error at position: {}", xml_reader.buffer_position());
+                Err(e)?;
+            }
+            _ => {}
+        }
+
+        buf.clear();
+
+        if let Some(ref pb) = progress_bar {
+            let cur_file_position = xml_reader.buffer_position() as u64;
+            if cur_file_position > pb.position() + update_progress_after_chunk {
+                pb.set_position(cur_file_position);
+            }
+        };
+    }
+
+    if !products_bucket.is_empty() {
+        let processed_products_stat = sync_products_chunk(
+            conn, &products_bucket, opts, &date_modified
+        )?;
+        stat.updated_price += processed_products_stat.updated_price;
+        stat.updated_available += processed_products_stat.updated_available;
+        stat.inserted_products += processed_products_stat.inserted;
+        total_sync_duration += processed_products_stat.duration;
+    }
+
+    if let Some(ref pb) = progress_bar {
+        pb.finish();
+    };
+
+    if opts.mark_missing_unavailable {
+        stat.marked_as_unavailable = mark_missing_as_unavailable(conn, &all_offer_ids, opts)?;
+    }
+
+    stat.total_duration = start_processing_at.elapsed();
+    stat.parse_duration = stat.total_duration - total_sync_duration;
+
+    Ok(stat)
+}
---
+++ src/process.rs
@@ -0,0 +1,259 @@
+use chrono::NaiveDateTime;
+
+use diesel::connection::SimpleConnection;
+use diesel::mysql::MysqlConnection;
+use diesel::prelude::*;
+
+use failure::Error;
+
+use indicatif::{ProgressBar, ProgressStyle};
+
+use std::collections::{HashMap, HashSet};
+use std::time::{Duration, Instant};
+
+use crate::{CHUNK_SIZE, Opts};
+use crate::models::{self, AVAILABLE, NOT_AVAILABLE};
+use crate::parser::Offer;
+use crate::schema::{self, products};
+
+
+pub(crate) fn convert_offer_to_product(offer: Offer) -> Option<models::NewProduct> {
+    let name = if let Some(name) = offer.name {
+        name
+    } else {
+        return None;
+    };
+    let category_id = if let Some(cat_id) = offer.category_id {
+        cat_id
+    } else {
+        return None;
+    };
+    let price = if let Some(price) = offer.price {
+        price
+    } else {
+        return None;
+    };
+    Some(models::NewProduct {
+        offer_id: offer.offer_id.clone(),
+        hub_stock_id: offer.offer_id.clone(),
+        available: offer.available,
+        categoryId: category_id,
+        name,
+        price,
+        oldprice: offer.old_price,
+        currencyId: offer.currency_id,
+        description: offer.description,
+    })
+}
+
+#[derive(Default)]
+pub(crate) struct ProcessedProducts {
+    pub updated_price: u32,
+    pub updated_available: u32,
+    pub inserted: u32,
+    pub duration: Duration,
+}
+
+pub(crate) fn sync_products_chunk(
+    conn: &MysqlConnection,
+    parsed_products: &Vec<models::NewProduct>,
+    opts: &Opts,
+    date_modified: &NaiveDateTime,
+) -> Result<ProcessedProducts, Error> {
+    use crate::schema::products::dsl::{products as products_table};
+
+    let start_syncing_at = Instant::now();
+    let mut processed_products_stat = ProcessedProducts::default();
+
+    let offer_ids = parsed_products.iter()
+        .map(|p| p.offer_id.as_str())
+        .collect::<Vec<_>>();
+    let found_products = products_table
+        .filter(schema::products::hub_stock_id.eq_any(offer_ids))
+        .load::<models::Product>(conn)?;
+    let offer_id_to_found_product = found_products.iter()
+        .filter_map(|p| {
+            if let Some(ref hub_stock_id) = p.hub_stock_id {
+                Some((hub_stock_id.as_str(), p))
+            } else {
+                None
+            }
+        })
+        .collect::<HashMap<_, _>>();
+
+    let mut raw_update_queries = String::new();
+    for p in parsed_products {
+        match offer_id_to_found_product.get(p.hub_stock_id.as_str()) {
+            Some(found_product) => {
+                let mut should_update = false;
+                let mut update_product = models::ModProduct::default();
+                if Some(p.available) != found_product.available {
+                    processed_products_stat.updated_available += 1;
+                    if opts.update_available {
+                        update_product.available = Some(&p.available);
+                        should_update = true;
+                    }
+                }
+                if p.price != found_product.price ||
+                    p.oldprice != found_product.oldprice ||
+                    p.currencyId != found_product.currencyId
+                {
+                    processed_products_stat.updated_price += 1;
+                    if opts.update_price {
+                        update_product.price = Some(&p.price);
+                        update_product.oldprice = Some(p.oldprice.as_ref());
+                        update_product.currencyId = Some(p.currencyId.as_deref());
+                        should_update = true;
+                    }
+                }
+                if should_update {
+                    // println!("Updating product with offer_id={}: {:?}", p.offer_id, update_product);
+                    raw_update_queries.push_str("UPDATE `products` SET ");
+                    if let Some(available) = update_product.available {
+                        raw_update_queries.push_str(
+                            &format!("`available` = {}, ", available.to_string())
+                        );
+                    }
+                    if let Some(price) = update_product.price {
+                        raw_update_queries.push_str(
+                            &format!("`price` = {}, ", price.to_string())
+                        );
+                    }
+                    if let Some(oldprice) = update_product.oldprice {
+                        raw_update_queries.push_str(
+                            &format!("`oldprice` = {}, ", optional_to_sql(oldprice))
+                        );
+                    }
+                    if let Some(currency_id) = update_product.currencyId {
+                        raw_update_queries.push_str(
+                            &format!("`currencyId` = {}", optional_string_to_sql(currency_id))
+                        );
+                    }
+                    raw_update_queries.push_str(&format!(
+                        "`renew_date` = '{}' WHERE `id` = {};\n",
+                        &date_modified, found_product.id
+                    ));
+
+//                    update_product.renew_date = Some(&date_modified);
+//                    diesel::update(schema::products::table.find(found_product.id))
+//                        .set(&update_product)
+//                        .execute(conn)?;
+                }
+            }
+            None => {}
+        }
+    }
+    if !raw_update_queries.is_empty() {
+        conn.batch_execute(&raw_update_queries)?;
+    }
+
+    let insert_products = parsed_products.iter()
+        .filter(|&p| {
+            !offer_id_to_found_product.contains_key(p.hub_stock_id.as_str())
+        })
+        .collect::<Vec<_>>();
+    processed_products_stat.inserted += insert_products.len() as u32;
+    if !insert_products.is_empty() {
+        if opts.insert_new {
+            diesel::insert_into(products::table)
+                .values(insert_products)
+                .execute(conn)?;
+        }
+    }
+
+    processed_products_stat.duration += start_syncing_at.elapsed();
+
+    Ok(processed_products_stat)
+}
+
+fn optional_to_sql<T: ToString>(v: Option<&T>) -> String {
+    return if let Some(v) = v {
+        v.to_string()
+    } else {
+        "NULL".to_string()
+    }
+}
+
+fn optional_string_to_sql(s: Option<&str>) -> String {
+    return if let Some(s) = s {
+        format!(
+            "'{}'",
+            s
+            .replace(r"\", r"\\")
+            .replace("'", r"\'")
+            .replace(r#"""#, r#"\""#))
+    } else {
+        "NULL".to_string()
+    }
+}
+
+pub(crate) fn mark_missing_as_unavailable(
+    conn: &MysqlConnection,
+    all_offer_ids: &HashSet<String>,
+    opts: &Opts,
+) -> Result<u32, Error> {
+    use crate::schema::products::dsl;
+
+    let mut last_product_id = 0;
+    let mut missing_offer_ids = Vec::with_capacity(CHUNK_SIZE);
+    let mut marked_count: u32 = 0;
+
+    let progress = if !opts.no_progress {
+        let total_products = dsl::products.select(dsl::id)
+            .filter(dsl::available.eq(AVAILABLE))
+            .count()
+            .get_result::<i64>(conn)? as u64;
+        let pb = ProgressBar::new(total_products);
+        pb.set_style(
+            ProgressStyle::default_bar()
+                .template("[{elapsed_precise}] [{bar:40.cyan/blue}] {pos:>7}/{len:6} ({eta})")
+        );
+        Some((pb, total_products / 100))
+    } else {
+        None
+    };
+
+    let mut total_processed: u64 = 0;
+    loop {
+        let db_offers = dsl::products.select((dsl::id, dsl::hub_stock_id))
+            .filter(dsl::id.gt(last_product_id))
+            .filter(dsl::available.eq(AVAILABLE))
+            .order(dsl::id)
+            .limit(CHUNK_SIZE as i64)
+            .load::<(i32, Option<String>)>(conn)?;
+
+        if db_offers.is_empty() {
+            break;
+        }
+
+        total_processed += db_offers.len() as u64;
+        last_product_id = db_offers.last().unwrap().0;
+
+        for (_, db_offer_id) in db_offers {
+            if let Some(db_offer_id) = db_offer_id {
+                if !all_offer_ids.contains(&db_offer_id) {
+                    missing_offer_ids.push(db_offer_id);
+                }
+            }
+        }
+        if !missing_offer_ids.is_empty() {
+            diesel::update(dsl::products.filter(dsl::hub_stock_id.eq_any(&missing_offer_ids)))
+                .set(dsl::available.eq(NOT_AVAILABLE))
+                .execute(conn)?;
+            marked_count += missing_offer_ids.len() as u32;
+            missing_offer_ids.clear();
+        }
+
+        if let Some((ref pb, update_after_count)) = progress {
+            if pb.position() + update_after_count < total_processed {
+                pb.set_position(total_processed);
+            }
+        }
+    }
+
+    if let Some((pb, _)) = progress {
+        pb.finish();
+    }
+
+    Ok(marked_count)
+}
